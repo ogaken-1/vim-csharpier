248ae159b9d0edd0466d7ce4f78a7e5844ea6b7d
Kento Ogata <k.ogata1013@gmail.com>
2025-10-14 11:40:28 +0900

    refactor: Capture object instead of `denops.context`

--- denops/csharpier/main.ts
+++ denops/csharpier/main.ts
@@ -2,24 +2,18 @@ import { Denops } from "https://deno.land/x/denops_core@v6.0.5/mod.ts";
 import { resourceReady, Server } from "./server.ts";
 import * as vim from "jsr:@denops/std@8.1.0/function";
 import { g } from "jsr:@denops/std@8.1.0/variable";
-import { as, assert, ensure, is } from "jsr:@core/unknownutil@4.3.0";
+import { ensure, is } from "jsr:@core/unknownutil@4.3.0";
 import { applyTextEdits } from "https://deno.land/x/denops_lsputil@v0.9.4/mod.ts";
 import { echoerr } from "jsr:@denops/std@8.1.0/helper/echo";
 
-const isContext = is.ObjectOf({
-  servers: is.RecordOf(
-    as.Optional((x): x is Server => x instanceof Server),
-    is.String,
-  ),
-});
+type WorkDir = string;
 
 export function main(denops: Denops) {
-  denops.context.servers = {};
+  const servers: Record<WorkDir, Server> = {};
   denops.dispatcher = {
     startServer: async () => {
-      assert(denops.context, isContext);
       const cwd = await vim.getcwd(denops);
-      const server = denops.context.servers[cwd];
+      const server = servers[cwd];
       if (server != null) {
         return;
       }
@@ -30,15 +24,14 @@ export function main(denops: Denops) {
       if (!await resourceReady(cmd)) {
         return;
       }
-      denops.context.servers[cwd] = new Server(cmd, cwd);
+      servers[cwd] = new Server(cmd, cwd);
     },
     format: async (bufnr: unknown): Promise<void> => {
-      assert(denops.context, isContext);
       if (!is.Number(bufnr)) {
         return;
       }
       const cwd = await vim.getcwd(denops);
-      const server = denops.context.servers[cwd];
+      const server = servers[cwd];
       if (server == null) {
         return;
       }
